d1250b94187efac84d9fb6c3270320fbeff2428c
Andres Cuervo <andrescuervor@gmail.com>
2015-07-24 01:17:11 -0400

    Add a link externalization script

---
+++ source/javascripts/externalize.js
@@ -0,0 +1,25 @@
+/*doc
+  ---
+title: Externalize
+category: Javascript
+---
+The file externalize.js is a little piece of jQuery that goes through all links
+(`<a>` tags) and sets their 'target' attribute to `_blank` if they are links
+to another website, causing browsers to open those links in new tabs.
+
+```haml_example
+:markdown
+ * [Link to this page.] (#) This link will open in the current
+ tab because it directs to a page that is in the same host.
+ * [ A link to example.com ] (http://www.example.com) Since this link
+ *isn't* in the same host, it should open in a new tab.
+ ```
+ */
+$(window).load(function(){
+    $('a').each(function () {
+        if (this.host !== location.host) {
+            external
+            this.setAttribute('target', '_blank');
+        }
+    });
+});
