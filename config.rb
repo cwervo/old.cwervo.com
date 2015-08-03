26026e9825ef0bf770b81cb0cda1389bfe2ddb4e
Andres Cuervo <andrescuervor@gmail.com>
2015-08-02 23:53:46 -0400

    Update deploy method to rsync

--- config.rb
+++ config.rb
@@ -94,13 +94,17 @@ end
 
 # Deploy configuration
 activate :deploy do |deploy|
-    deploy.method = :git
-    # Optional Settings
-    deploy.remote   = 'git@github.com:AndresCuervo/andrescuervo.github.io.git' # remote name or git url, default: origin
-    deploy.branch   = 'master' # default: gh-pages
+    deploy.method = :rsync
+    deploy.host = "cwervo.com"
+    deploy.path = "/home/tinycarr/www"
+    deploy.user = "tinycarr"
+    # deploy.method = :git
+    # # Optional Settings
+    # deploy.remote   = 'git@github.com:AndresCuervo/andrescuervo.github.io.git' # remote name or git url, default: origin
+    # deploy.branch   = 'master' # default: gh-pages
     # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
 
-    commit_message = `git log --pretty=oneline --abbrev-commit -n1`.strip
-
-    deploy.commit_message = commit_message
+    # commit_message = `git log --pretty=oneline --abbrev-commit -n1`.strip
+    #
+    # deploy.commit_message = commit_message
 end
